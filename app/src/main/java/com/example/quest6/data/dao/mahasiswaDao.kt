3748f42cac5d547abfce2c8c9803db3462a0f9b0
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-17 00:22:26 +0700

    untuk memperbarui data mahasiswa dalam database

--- app/src/main/java/com/example/quest6/data/dao/mahasiswaDao.kt
+++ app/src/main/java/com/example/quest6/data/dao/mahasiswaDao.kt
@@ -2,10 +2,14 @@ package com.example.quest6.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Update
 import com.example.quest6.data.entity.mahasiswa
 
 @Dao
 interface mahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: mahasiswa)
 }
