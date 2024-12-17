b85c3fda49ab42aa05cb904114365b65f49db8a6
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-17 13:38:46 +0700

    mengambil dan mengelola data mahasiswa dan menyediakan data ui ke dalam bentukk stateflow

--- app/src/main/java/com/example/quest6/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/quest6/viewmodel/HomeMhsViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.quest6.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.quest6.data.entity.mahasiswa
 import com.example.quest6.repository.repositoryMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 data class HomeUiState(
     val listMhs: List<mahasiswa> = listOf(),
@@ -13,3 +19,37 @@ data class HomeUiState(
     val isError: Boolean = false,
     val errorMessages: String = ""
 )
+
+class HomeMhsViewModel(
+    private val repositoryMhs: repositoryMhs
+) : ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessages = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true
+            )
+        )
+}
