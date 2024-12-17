912aacf036d827e2e164ab26ec54fa973c3f35c7
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-17 15:20:43 +0700

    untuk mendefinisikan rute-rute navigasi dalam aplikasi Android

---
+++ app/src/main/java/com/example/quest6/ui/customwidget/alamatnavigation.kt
@@ -0,0 +1,26 @@
+package com.example.quest6.ui.customwidget
+
+interface alamatnavigation {
+    val route: String
+
+    object DestinasiHome : alamatnavigation {
+        override val route = "home"
+    }
+    object DestinasiDetail : alamatnavigation {
+        override val route = "detail"
+        const val NIM = "nim"
+        val routeWithArgs = "$route/{$NIM}"
+    }
+
+    object DestinasiUpdate : alamatnavigation {
+        override val route = "update"
+        const val NIM = "nim"
+        val routeWithArgs = "$route/{$NIM}"
+    }
+
+    object DestinasiEdit : alamatnavigation {
+        override val route = "edit"
+        const val NIM = "nim"
+        val routeWithArgs = "$route/{$NIM}"
+    }
+}
