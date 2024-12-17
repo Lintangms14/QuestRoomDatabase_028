53589042c9728af5a6c0b8ddb2f5ad62b778913c
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-17 13:26:32 +0700

    untuk mengelola data mahasiswa

--- app/src/main/java/com/example/quest6/repository/repositoryMhs.kt
+++ app/src/main/java/com/example/quest6/repository/repositoryMhs.kt
@@ -1,7 +1,16 @@
 package com.example.quest6.repository
 
 import com.example.quest6.data.entity.mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface repositoryMhs {
     suspend fun insertMhs(mahasiswa: mahasiswa)
+
+    fun getAllMhs(): Flow<List<mahasiswa>>
+
+    fun getMhs(nim: String): Flow<mahasiswa>
+
+    suspend fun deleteMhs(mahasiswa: mahasiswa)
+
+    suspend fun updateMhs(mahasiswa: mahasiswa)
 }
